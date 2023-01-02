8832a41c823d2b2af163a3baeddf6db9ad0a1709
Kelamir <kelamir@protonmail.com>
2023-01-02 19:57:33 +0200

    Fix url paths

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
+
 <h5>Kelamir's blog</h5>
 
+
 <ul>
-    <li> <a href="Home">Home</a> </li>
-    <li> <a href="Posts">Posts</a> </li>
-    <li> <a href="Contact">Contact</a> </li>
-    <li> <a href="About">About</a> </li>
+    <li> <a href="/home">Home</a> </li>
+    <li> <a href="/hosts">Posts</a> </li>
+    <li> <a href="/contact">Contact</a> </li>
+    <li> <a href="/about">About</a> </li>
 </ul>
+
+
 
 <style>
     ul { display: flex; flex-direction: row}
