3c5f7e6b2d7ce70f23bd048d4a7568bf8a87847c
Kelamir <kelamir@protonmail.com>
2023-01-02 18:02:26 +0200

    Set up a layout that will display links to navigate the website on all pages.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<h5>Kelamir's blog</h5>
+
+<ul>
+    <li> <a href="Home">Home</a> </li>
+    <li> <a href="Posts">Posts</a> </li>
+    <li> <a href="Contact">Contact</a> </li>
+    <li> <a href="About">About</a> </li>
+</ul>
+
+<style>
+    ul { display: flex; flex-direction: row}
+    li { display: flex;  margin-left: 5px}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,2 @@
-<h1>Welcome to my Blog!</h1>
+
+<ul></ul>
