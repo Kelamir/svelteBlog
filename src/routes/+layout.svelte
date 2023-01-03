3c9aba363176b363fd7b1b1f8562dffb47dc9c09
Kelamir <kelamir@protonmail.com>
2023-01-03 22:18:33 +0200

    Use <nav> for navigating the blog, remove subpar transition. and increase heading size.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,11 @@
-<script>
-    import { fade } from 'svelte/transition';
-    let transitioning = true;
 
-    function toggleTransition() {
-        transitioning = !transitioning;
-    }
-</script>
+<h2>Kelamir's blog</h2>
 
+<nav>
+    <a href="/">Home</a>
+    <a href="/posts">Posts</a>
+    <a href="/contact">Contact</a>
+    <a href="/about">About</a>
+</nav>
 
-
-<h5 transition:fade>Kelamir's blog</h5>
-
-<ul>
-    <li on:click={toggleTransition}> <a href="/">Home</a> </li>
-    <li on:click={toggleTransition}> <a href="/posts">Posts</a> </li>
-    <li on:click={toggleTransition}> <a href="/contact">Contact</a> </li>
-    <li on:click={toggleTransition}> <a href="/about">About</a> </li>
-</ul>
-
-{#if transitioning}
-    <div transition:fade>
-        <slot />
-    </div>
-{/if}
-
-<style>
-    ul { display: flex; flex-direction: row}
-    li { display: flex;  margin-left: 5px}
-
-</style>
+<slot />
