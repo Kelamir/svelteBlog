d596871632d99ed02ce68bc4f325f388f26cad47
Kelamir <kelamir@protonmail.com>
2023-01-03 12:36:55 +0200

    Fix layout so it includes pages' inherent contents, add example contents, display poems properly

---
+++ src/routes/posts/+page.svelte
@@ -0,0 +1,47 @@
+<h2>Posts</h2>
+
+<h3>Sun</h3>
+
+<p>Oh, golden sun in the sky,
+    Your rays bring the warmth and light,
+    Your wonders are so high,
+    You bring joy and delight.
+
+    Your beauty is so divine,
+    Your light is so bright,
+    Your rays give us life and shine,
+    You bring us hope and insight.
+
+    Your warmth brings us peace,
+    Your joy fills our heart,
+    Your love is never ceasing,
+    As we look to a new start.
+
+    Your glow will never fade,
+    Your rays forever shine,
+    Your love is never-ending,
+    No matter the time.
+
+    Oh, golden sun in the sky,
+    Your wonders are so high,
+    Your rays bring the warmth and light,
+    Your beauty is divine.
+</p>
+
+<h3>Asynkrunes</h3>
+<p>1. Wat iz asynkrunes?
+    2. How doez asynkrunes werk?
+    3. How iz asynkrunes diffrent frum synskrunes?
+    4. How kan I impruuv asynkrunes parefurmanse?
+    5. Wat duz a asynkrunes kalbak luk lyk?
+    6. Wat iz a asynkrunes promis?
+    7. How duz asynkrunes handul erruur handeling?
+    8. Wat iz a asynkrunes await operatoor?
+    9. How kan I debub asynkrunes kode?
+    10. How kan I impruuv mei asynkrunes kode?
+</p>
+
+<style>
+    p { white-space: pre}
+</style>
+
