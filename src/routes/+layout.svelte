361b375f653cb2507d4d5bd747e78e8e2a2f7671
Kelamir <kelamir@protonmail.com>
2023-01-03 13:58:31 +0200

    Add a fade transition between pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,26 @@
 <script>
     import { fade } from 'svelte/transition';
-    let visible = true;
+    let transitioning = true;
 </script>
 
 
 
-<input type="checkbox" bind:checked={visible}>
+<input type="checkbox" bind:checked={transitioning}>
 
-
-{#if visible}
     <h5 transition:fade>Kelamir's blog</h5>
 
-    <ul transition:fade>
-        <li> <a href="/home">Home</a> </li>
-        <li> <a href="/posts">Posts</a> </li>
-        <li> <a href="/contact">Contact</a> </li>
-        <li> <a href="/about">About</a> </li>
+    <ul>
+        <li on:click={transitioning = !transitioning}> <a href="/home">Home</a> </li>
+        <li on:click={transitioning = !transitioning}> <a href="/posts">Posts</a> </li>
+        <li on:click={transitioning = !transitioning}> <a href="/contact">Contact</a> </li>
+        <li on:click={transitioning = !transitioning}> <a href="/about">About</a> </li>
     </ul>
+
+{#if transitioning}
+    <div transition:fade>
+        <slot />
+    </div>
 {/if}
-
-<slot />
-
 
 <style>
     ul { display: flex; flex-direction: row}
