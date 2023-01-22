61330af90f40160ee1ecd0ccaac620c42aedef1d
Kelamir <kelamir@protonmail.com>
2023-01-22 23:57:35 +0200

    Display in a flex grid

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,20 +2,22 @@
   import "../app.css";
 </script>
 
-<h2 class="text-2xl font-bold underline">Kelamir's blog</h2>
 
-<nav>
-    <a href="/">Home</a>
-    <a href="/posts">Posts</a>
-    <a href="/contact">Contact</a>
-    <a href="/about">About</a>
-</nav>
+<div class="flex justify-center">
+  <h2 class="text-2xl font-bold underline">Kelamir's blog</h2>
+  <nav>
+      <a href="/">Home</a>
+      <a href="/posts">Posts</a>
+      <a href="/contact">Contact</a>
+      <a href="/about">About</a>
+  </nav>
+<slot class="mx self-center"/>
+</div>
 
-<slot />
 
 
 <style lang="postcss">
   :global(html) {
-    background-color: theme(colors.gray.100);
+    background-color: theme(colors.yellow.100);
   }
 </style>
