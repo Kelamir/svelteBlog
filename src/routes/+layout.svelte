db9479c18e2fb53293391f326b300afa70a750f3
Kelamir <kelamir@protonmail.com>
2023-01-23 15:39:02 +0200

    Addanimations to buttons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,10 @@
 <h2 class="text-2xl font-bold underline first-letter:text-6xl first-letter:font-bold">Kelamir's blog</h2>
 
 <div class="grid grid-cols-5 w-1/4 p-3">
-  <a class="border bg-gray-300 text-zink-300 hover:text-zink-700 text-center" href="/">Home</a>
-  <a class="border bg-gray-300 text-zink-300 hover:text-zink-700 text-center" href="/posts">Posts</a>
-  <a class="border bg-gray-300 text-zink-300 hover:text-zink-700 text-center" href="/contact">Contact</a>
-  <a class="border bg-gray-300 text-zink-300 hover:text-zink-700 text-center" href="/about">About</a>
+  <a class="border bg-gray-300 text-zink-300 hover:text-zink-700 text-center hover:animate-spin" href="/">Home</a>
+  <a class="border bg-gray-300 text-zink-300 hover:text-zink-700 text-center hover:animate-ping" href="/posts">Posts</a>
+  <a class="border bg-gray-300 text-zink-300 hover:text-zink-700 text-center hover:animate-pulse" href="/contact">Contact</a>
+  <a class="border bg-gray-300 text-zink-300 hover:text-zink-700 text-center hover:animate-bounce" href="/about">About</a>
 
 </div>
 
